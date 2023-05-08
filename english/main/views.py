42644a0b494b4ad3ccf23c21edd191aabc389852
Komilast <Komilast400@gmail.com>
2023-05-08 16:04:27 +0300

    Определение главного шаблона и ряд других работ

--- english/main/views.py
+++ english/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    args = {
+        'title': 'Изучение английского'
+    }
+    return render(request, 'main/index.html', args)
