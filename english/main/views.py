3d438efbab8985ec3fd25cc39b4585892ac3c800
Komilast <Komilast400@gmail.com>
2023-05-09 19:18:31 +0300

    Создан навбар

--- english/main/views.py
+++ english/main/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     args = {
-        'title': 'Изучение английского'
+        'title': 'Изучение английского',
+        'navbar_list': ['Моё обучение', 'Форум', 'Карточки слов', 'Уроки', 'Другое']
     }
     return render(request, 'main/index.html', args)
